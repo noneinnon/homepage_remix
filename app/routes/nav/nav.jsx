ada6423518b47d8308f58a274f5809f2630f0231
Anton Baranov <44464480+noneinnon@users.noreply.github.com>
2022-08-05 12:52:28 +0300

    adds netlify

--- app/routes/nav/nav.jsx
+++ app/routes/nav/nav.jsx
@@ -9,6 +9,7 @@ export default function Nav() {
         <nav className="flex justify-end gap-2 px-5 py-2">
             <a className={applyActiveLinkClass('/', pathname)} href="/">home</a>
             <a className={applyActiveLinkClass('/blog', pathname)} href="/blog">blog</a>
+            <a className={applyActiveLinkClass('/projects', pathname)} href="/projects">projects</a>
         </nav>
     )
 }
---
+++ app/routes/projects.jsx
@@ -0,0 +1,10 @@
+import Nav from "./nav";
+
+export default function Blog() {
+  return (
+    <>
+      <Nav />
+      <main>blog posts go here</main>
+    </>
+  );
+}
